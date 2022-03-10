124e5438a6fdd3f4bca2bc8cb18338fb79090dad
VVD <zywvvd@mail.ustc.edu.cn>
2022-03-10 23:42:03 +0800

    update

--- OpenCV/Chapter 6/part1.py
+++ OpenCV/Chapter 6/part1.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import mtutils as mt
 from mtutils import PIS
+from sqlalchemy import false
 
 
 if __name__ == '__main__':
@@ -67,6 +68,56 @@ if __name__ == '__main__':
     # cv2.fillPoly(canvas, [porints1, porints2], color)
     # PIS(canvas)
 
+
+    ## cv2.line()
+    # canvas = np.zeros([300, 300, 3], dtype='uint8')
+    # pt1 = [12, 186]
+    # pt2 = [192, 399]
+    # color = [255, 255, 255]
+    # cv2.line(canvas, pt1, pt2, color, thickness=5) 
+    # PIS(canvas)
+
+
+    ## cv2.rectangle() 
+    # canvas = np.zeros([300, 300, 3], dtype='uint8')
+    # pt1 = [12, 186]
+    # pt2 = [192, 199]
+    # color = [255, 255, 255]
+    # cv2.rectangle(canvas, pt1, pt2, color, thickness=5) 
+    # PIS(canvas)
+
+
+    ## cv2.polyLines()
+    # canvas = np.zeros([300, 300, 3], dtype='uint8')
+    # pts1 =np.array([[12, 186], [56, 96], [156, 198], [222, 29]])
+    # pts2 = np.array([[192, 199], [98, 65], [299, 365], [55, 99]])
+    # color = [255, 255, 255]
+    # cv2.polylines(canvas, [pts1, pts2], True, color, thickness=5)
+    # PIS(canvas)
+
+
+    ## cv2.putText() 
+    # canvas = np.zeros([200, 600, 3], dtype='uint8')
+    # text = 'hello world! 表独立兮山之上'
+    # origin = [20, 100]
+    # fontFace=cv2.FONT_HERSHEY_TRIPLEX
+    # fontScale = 1.3
+    # color = [255, 255, 0]
+    # thickness = 2
+    # lineType = 8
+    # bottomLeftOrigin = False
+    # cv2.putText(canvas, text, origin, fontFace, fontScale, color, thickness, lineType, bottomLeftOrigin) 
+    # PIS(canvas)
+
+
+    ##  cv2.getTextSize() 
+    # text = 'hello world!'
+    # origin = [20, 100]
+    # fontFace=cv2.FONT_HERSHEY_TRIPLEX
+    # fontScale = 1.3
+    # thickness = 2
+
+    # res = cv2.getTextSize(text, fontFace, fontScale, thickness) 
 
     pass
 
