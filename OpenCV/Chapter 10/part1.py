4eb8d23981ad7df8ac36d2b518e3dcea6bb8af39
Zhang Yiwei <zywvvd@mail.ustc.edu.cn>
2022-03-12 19:47:53 +0800

    update

---
+++ OpenCV/Chapter 10/part1.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import mtutils as mt
+from mtutils import PIS
+from sqlalchemy import false
+
+
+if __name__ == '__main__':
+    
+    
+    ## cv2.circle()
+    # canvas = np.zeros([400, 400, 3], dtype='uint8')
+    # center = [150, 150]
+    # radius = 50
+    # color = [255, 255, 0]
+    # thickness = 4
+    # lineType = 8
+    # shift = 0
+    # cv2.circle(canvas, center, radius, color, thickness, lineType, shift)
+    # PIS(canvas)
+    
+
+    pass
+
